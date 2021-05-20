03561330232dc50c45ec25617ce7e9b7bbc35b90
Edison Cai <edisoncai2001@gmail.com>
2021-05-20 12:31:00 +0100

    adding some more sounds and initialising operator and clear buttons

--- JapaneseCalculator/app/src/main/java/com/example/japanesecalculator/MainActivity.java
+++ JapaneseCalculator/app/src/main/java/com/example/japanesecalculator/MainActivity.java
@@ -7,10 +7,17 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine, btnZero;
+    private TextView txtNumber;
+    private Button btnDivide, btnMultiply, btnSubtract, btnAdd, btnEquals;
+    private Button btnClear;
+
+    String number = "";
+    boolean cleared = true;
 
     MediaPlayer oneSoundMP, twoSoundMP, threeSoundMP, fourSoundMP, fiveSoundMP, sixSoundMP, sevenSoundMP, eightSoundMP, nineSoundMP, zeroSoundMP;
 
@@ -30,7 +37,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         nineSoundMP = MediaPlayer.create(this, R.raw.nine);
         zeroSoundMP = MediaPlayer.create(this, R.raw.zero);
 
-        initialiseButtons();
+        initialiseViews();
 
         btnOne.setOnClickListener(this);
         btnTwo.setOnClickListener(this);
@@ -43,7 +50,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnNine.setOnClickListener(this);
         btnZero.setOnClickListener(this);
     }
-    public void initialiseButtons(){
+    public void initialiseViews(){
         btnOne = findViewById(R.id.btnOne);
         btnTwo = findViewById(R.id.btnTwo);
         btnThree = findViewById(R.id.btnThree);
@@ -54,6 +61,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnEight = findViewById(R.id.btnEight);
         btnNine = findViewById(R.id.btnNine);
         btnZero = findViewById(R.id.btnZero);
+
+        txtNumber = findViewById(R.id.txtNumber);
+
+        btnDivide = findViewById(R.id.btnDivide);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnSubtract = findViewById(R.id.btnSubtract);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        btnClear = findViewById(R.id.btnClear);
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -62,36 +78,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.btnOne:
                 oneSoundMP.start();
+                insertNumber("1");
                 break;
             case R.id.btnTwo:
                 twoSoundMP.start();
+                insertNumber("2");
                 break;
             case R.id.btnThree:
                 threeSoundMP.start();
+                insertNumber("3");
                 break;
             case R.id.btnFour:
                 fourSoundMP.start();
+                insertNumber("4");
                 break;
             case R.id.btnFive:
                 fiveSoundMP.start();
+                insertNumber("5");
                 break;
             case R.id.btnSix:
                 sixSoundMP.start();
+                insertNumber("6");
                 break;
             case R.id.btnSeven:
                 sevenSoundMP.start();
+                insertNumber("7");
                 break;
             case R.id.btnEight:
                 eightSoundMP.start();
+                insertNumber("8");
                 break;
             case R.id.btnNine:
                 nineSoundMP.start();
+                insertNumber("9");
                 break;
             case R.id.btnZero:
                 zeroSoundMP.start();
+                insertNumber("0");
                 break;
             default:
                 break;
         }
     }
+
+    public void insertNumber(String enteredNumber){
+        //Do nothing if the number inputted is 0 and it's cleared already
+        if(cleared && enteredNumber.equals("0")){
+            return;
+        }
+        number += enteredNumber;
+        txtNumber.setText(number);
+        cleared = false;
+    }
+
 }
